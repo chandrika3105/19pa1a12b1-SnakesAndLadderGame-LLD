5e596d9a19a49279d00f97bdcdeee06e7eea8106
chandrika3105 <72602440+chandrika3105@users.noreply.github.com>
2021-12-12 19:22:33 +0530

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,3 +1,8 @@
+/*Dice class is used to generate random number on a dice throw of player
+Here code is written in such a way that if the user throws 6 one time he gets another chance and if he throws 2 times again he get a chance but if he throws 3rd time his turn will be cancelled and next person turn occurs*/
+
+
+
 import java.lang.Math;
 public class Dice{
 	private int nofDice;
